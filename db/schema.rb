beb9934d4d08e39394fed247b2edc7027db2ec30
Tana Jukes <tana.jukes@gmail.com>
2016-07-10 14:16:11 -0700

    Add Bookings to database
    Bookings describe a specific job for a bike repair scheduled between a cyclist and a mechanic. Each booking belongs to both a cyclist and a mechanic, and contains the following information:
    - a description, to be set by the cyclist when they request the booking
    - a list of the services requested, to be stored as JSON
    - a list of comments that the mechanic can add to as the job progresses, to be stored as JSON
    - the status of the booking (open / closed / in progress) to be stored as text

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160710210024) do
+ActiveRecord::Schema.define(version: 20160710210934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer  "cyclist_id"
+    t.integer  "mechanic_id"
+    t.text     "description"
+    t.string   "status"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["cyclist_id"], name: "index_bookings_on_cyclist_id", using: :btree
+    t.index ["mechanic_id"], name: "index_bookings_on_mechanic_id", using: :btree
+  end
 
   create_table "services", force: :cascade do |t|
     t.integer  "mechanic_id"
