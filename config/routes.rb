84ebf9c58adade3fe10751f2ccae721a18bffe96
Tana Jukes <tana.jukes@gmail.com>
2016-07-14 14:03:54 -0700

    Add scoped review-related resources for mechanics
    This commit adds two sets of resources for mechanics:
    - Routes to allow mechanics to view reviews written about them (reviews_of_mechanics)
    - Routes to allow mechanics to view, create, and edit reviews they have written of cyclists
    Reviews should not be deleted, but they can be edited to allow for 'course correction' based on any follow-up that might happen after a booking is complete.
    
    While creating two sets of review-related routes makes for some long and complex resource paths, we have chosen to keep that complexity in the routes rather putting more complex logic in the controllers or views. For example, mechanics can see reviews written about themselves, but shouldn't be able to edit them.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     resources :bookings, only: [:index, :edit, :update, :show] do
       resources :comments, except: :destroy
     end
+    resources :review_of_mechanic, only: [:index, :show]
+    resources :review_of_cyclist, except: :destroy
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
