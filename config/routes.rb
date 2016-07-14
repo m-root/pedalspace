3339b8297a50774117d34b186447e4f53bfc601d
Tana Jukes <tana.jukes@gmail.com>
2016-07-14 13:27:20 -0700

    Add scoped bookings routes for mechanics
    Mechanics will interact with bookings differently than cyclists will. Cyclists will always be the ones to initiate bookings - therefore mechanics have no routes for creation of bookings. Mechanics should only view and update bookings. Updating bookings means changing their status, or by commenting on them.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   resources :mechanics do
     resources :services
+    resources :bookings, only: [:index, :edit, :update, :show]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
