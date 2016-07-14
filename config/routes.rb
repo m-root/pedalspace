a208663ef17a02fe087ee606b878149423284416
Tana Jukes <tana.jukes@gmail.com>
2016-07-14 13:16:02 -0700

    Allow mechanics to manage their services
    Add scoped services routes for mechanics. This creates the routes necessary for mechanics to manage the services they offer (add, remove, edit, etc).

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   resources :cyclists
 
-  resources :mechanics
+  resources :mechanics do
+    resources :services
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
