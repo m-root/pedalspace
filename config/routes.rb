4dec2d1539171c46addf114d0c6c31272bafe663
jairusstreight <jairus@uvic.ca>
2016-07-13 20:23:49 -0700

    Added controller for Comments
    Views specified for Index, Show, New, Edit, Create and Update were created, as routes for the controller.
    There will be no Delete route for Comments.
    
    Additionally stylesheets, and tests were created for this controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'comments/index'
+
+  get 'comments/new'
+
+  get 'comments/create'
+
+  get 'comments/edit'
+
+  get 'comments/update'
+
+  get 'comments/show'
+
   get 'services/index'
 
   get 'services/new'
