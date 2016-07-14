a63216a74323ddc63228c4a77524091e22f4ceb3
Tana Jukes <tana.jukes@gmail.com>
2016-07-14 13:13:58 -0700

    Add default routes for mechanics
    Add all basic routes for mechanics by invoking 'resources'. No scoped resources defined yet.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   resources :cyclists
 
+  resources :mechanics
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
