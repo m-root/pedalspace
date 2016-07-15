9832691ab127887c3401947ca76f9e1d4aac89b0
Tana Jukes <tana.jukes@gmail.com>
2016-07-15 13:12:04 -0700

    Add actions for updating Reviews of Mechanic
    The 'edit' action simply returns the review instance to be edited, which is found by taking the id from the params.
    
    The 'update' action updates the review by taking in the same strong params as are used in creating a new review. If the update is successful, redirect to the show the mechanic's profile, which is where the reviews should be displayed.

--- app/controllers/review_of_mechanics_controller.rb
+++ app/controllers/review_of_mechanics_controller.rb
@@ -18,9 +18,17 @@ class ReviewOfMechanicsController < ApplicationController
   end
 
   def edit
+    @review = ReviewOfMechanic.find(params[:id])
   end
 
   def update
+    @review = ReviewOfMechanic.find(params[:id])
+
+    if @review.update_attributes(review_params)
+      redirect_to mechanic_path
+    else
+      render :edit
+    end
   end
 
   def show
