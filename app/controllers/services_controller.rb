34e5cd8f7682cd1f05559c118fabdc342cc4f6eb
Tana Jukes <tana.jukes@gmail.com>
2016-07-15 13:26:07 -0700

    Add actions for creating mechanics Services
    The 'new' action simply returns a new (empty) instantiation of the Service model.
    
    The 'create' action takes in parameters from the session and the user to create a new service:
    - The ID of the mechanic adding a service is taken from the session
    - Strong params are defined to accept params containing details for the service (service name & price)
    - The service is built via the mechanic object which automatically attaches it to that mechanic
    
    If the service is successfully saved to the database, redirect to the mechanic's list of services.
    If it isn't successfully saved, re-render the form for creating a new service (ideally the initial details entered should be preserved and error messages should be displayed).

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -3,9 +3,18 @@ class ServicesController < ApplicationController
   end
 
   def new
+    @service = Service.new
   end
 
   def create
+    @mechanic = Mechanic.find(session[:id])
+    @service = @mechanic.services.build(service_params)
+
+    if @service.save
+      redirect_to mechanic_services_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -19,4 +28,11 @@ class ServicesController < ApplicationController
 
   def delete
   end
+
+  protected
+
+  def service_params
+    params.require(:service).permit(:service_name, :service_price)
+  end
+
 end
