011c00175f1adb147f2fe376beeb8e0744aa0196
Trevor Morrison <trevorjames.morrison@gmail.com>
2016-07-15 19:21:40 +0000

    Add Cyclist controller
    The Cyclist controller enables actions specific to the Cyclist user subclass.  Details on each action are as follows:
    -New: instantiates new Cyclist object.
    -Create: creates new Cyclist object, sets parameters as per user input.  Sets avg_rating to default value of 0.
    -Edit: Finds Cyclist object based on ID number.
    -Update: Find Cyclst object based on ID number.  Updates parameters as per user input.  Redirects to Cyclist profile on successful save.
    -Show: Finds Cyclist object based on ID number.
    -Delete: Fiinds Cyclist object based on ID number.  Redirects to Mechanics path on successful deletion.

--- app/controllers/cyclists_controller.rb
+++ app/controllers/cyclists_controller.rb
@@ -1,22 +1,52 @@
 class CyclistsController < ApplicationController
-  def index
-  end
 
   def new
+    @cyclist = Cyclist.new
   end
 
   def create
+    @cyclist = Cyclist.new(cyclist_params)
+    @cyclist.avg_rating = 0
+
+    if @cyclist.save
+      session[:id] = @cyclist.id
+      redirect_to cyclist_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @cyclist = Cyclist.find(params[:id])
   end
 
   def update
+    @cyclist = Cyclist.find(params[:id])
+
+    if @cyclist.update_attributes(cyclist_params)
+      redirect_to cyclist_path
+    else
+      render :edit
+    end
   end
 
   def show
+    @cyclist = Cyclist.find(params[:id])
   end
 
   def delete
+    @cyclist = Cyclist.find(params[:id])
+    if @cyclist.destroy
+      redirect_to mechanics_path
+    else
+      redirect_to cyclist_path
+    end
   end
+
+  protected
+
+  def cyclist_params
+    params.require(:cyclist).permit(:first_name, :last_name, :photo, :email, :password, :bio)
+  end
+
 end
