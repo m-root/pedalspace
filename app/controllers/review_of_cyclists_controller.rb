e99e9af6f763df53a715d883a438e670082bae58
Tana Jukes <tana.jukes@gmail.com>
2016-07-15 12:47:08 -0700

    Add action for creating Reviews of Cyclists
    The 'new' action simply returns a new (empty) instantiation of the ReviewOfCyclist model.
    
    The 'create' action takes in parameters from the session and the user to create a new review of a mechanic in the following way:
    - Strong params are defined in a protected method to take in the ID of the cyclist being reviewed (this should be provided via a hidden field in the review form)
    - The strong params also takes in the other info for the review: rating, comments, and whether or not they provided the expected service at the expected price
    - The ID of the mechanic making the review is taken from the session
    - The review is built via the cyclist object, setting the review details (including the ID of the mechanic the review is for) from the params.

--- app/controllers/review_of_cyclists_controller.rb
+++ app/controllers/review_of_cyclists_controller.rb
@@ -1,11 +1,21 @@
 class ReviewOfCyclistsController < ApplicationController
+
   def index
   end
 
   def new
+    @review = ReviewOfCyclist.new
   end
 
   def create
+    @mechanic = Mechanic.find(session[:id])
+    @review = @mechanic.review_of_cyclist.build(review_params)
+
+    if @review.save
+      redirect_to cyclist_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -16,4 +26,11 @@ class ReviewOfCyclistsController < ApplicationController
 
   def show
   end
+
+  protected
+
+  def review_params
+    params.require(:review_of_cyclist).permit(:cyclist_id, :rating, :service_expected, :price_expected, :comments)
+  end
+
 end
