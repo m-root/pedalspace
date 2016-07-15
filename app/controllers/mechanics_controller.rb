3924eef08ee070038534d5ed01c562807d787a4d
Trevor Morrison <trevorjames.morrison@gmail.com>
2016-07-15 19:41:44 +0000

    Add Mechanic controller
    The Mechanic controller enables actions specific to the Mechanic user subclass.  Details on each action are as follows:
    
    -Index: Finds mechanics based on user search paramters for name and city.
    -New: instantiates new Mechanic object
    -Create: creates new Mechcreates new Cyclist object, sets parameters as per user input.  Sets avg_rating to default value of 0.  Redirects to Mechanic profile on successful save.
    -Edit: Finds Mechanic based on ID number.
    -Update: Finds Mechanic based on ID number.  Updates parameters as per user input.  Redirects to Mechanic profile on successful save.
    -Show: Finds Mechanic based on ID number.
    -Delete: Finds Mechanic based on ID number.  Redirects to Mechanics path on successful deletion.

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -1,22 +1,60 @@
 class MechanicsController < ApplicationController
+
   def index
+    @mechanics = Mechanic.search(search_params)
   end
 
   def new
+    @mechanic = Mechanic.new
   end
 
   def create
+    @mechanic = Mechanic.new(mechanic_params)
+    @mechanic.avg_rating = 0
+
+    if @mechanic.save
+      session[:id] = @mechanic.id
+      redirect_to mechanic_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @mechanic = Mechanic.find(params[:id])
   end
 
   def update
+    @mechanic = Mechanic.find(params[:id])
+
+    if @mechanic.update_attributes(mechanic_params)
+      redirect_to mechanic_path
+    else
+      render :edit
+    end
   end
 
   def show
+    @mechanic = Mechanic.find(params[:id])
   end
 
   def delete
+    @mechanic = Mechanic.find(params[:id])
+    if @mechanic.destroy
+      redirect_to mechanics_path
+    else
+      redirect_to mechanic_path
+    end
   end
+
+  protected
+
+  def mechanic_params
+    params.require(:mechanic).permit(:first_name, :last_name, :photo, :email, :password, :bio, :street_address, :city, :postal_code)
+  end
+
+  def search_params
+    params.permit(:first_name, :last_name, :city)
+  end
+
 end
