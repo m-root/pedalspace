daa326bf6e8ba1777cbff37c1e1f61bd427be7df
Trevor Morrison <trevorjames.morrison@gmail.com>
2016-07-15 20:17:32 +0000

    Add Comment controller
    The Comment controller enables actions for a comment made by a Mechanic specific to a particular Booking.  Details on each action are as follows:
    -New: instantiates a Comment object
    -Create: creates a new Comment associated with a particular Booking.  Redirects to the Mechanics Booking list on successful save.
    -Edit: Finds a Comment based on ID.
    -Update: Finds a Comment based on ID.  Redirects to the Mechanics Booking list on successful update.
    -Show: Finds a Comment based on ID.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,42 @@
 class CommentsController < ApplicationController
-  def index
-  end
 
   def new
+    @comment = Comment.new
   end
 
   def create
+    @booking = Booking.find(params[:id])
+    @comment = @booking.comments.build(comment_params)
+
+    if @comment.save
+      redirect_to mechanic_booking_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @comment = Comment.find(params[:id])
   end
 
   def update
+    @comment = Comment.find(params[:id])
+
+    if @comment.update_attributes(comment_params)
+      redirect_to mechanic_booking_path
+    else
+      render :edit
+    end
   end
 
   def show
+    @comment = Comment.find(params[:id])
   end
+
+  protected
+
+  def comment_params
+    params.require(:comment).permit(:booking_id, :content)
+  end
+
 end
