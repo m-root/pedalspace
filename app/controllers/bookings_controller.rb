1ec8b00c4dffbe09905f8225e57643e67c8b7c91
Trevor Morrison <trevorjames.morrison@gmail.com>
2016-07-15 20:01:37 +0000

    Add Booking controller
    The Booking controller enables actions for a scheduled arrangement between a Cyclist and Mechanic.  Details on each action are as follows:
    -New: instantiates new Booking object
    -Create: creates new Booking.  Sets cyclist_id by session[:id] (always a Cyclist).  Sets default status to Pending.  Redirects to Cyclist Booking path on successful save.
    -Edit: Finds a Booking based on ID.
    -Update: Finds a Booking based on ID.  Updates parameters as per user input.  Redirects to root path on successful save.
    -Show: Finds a Booking based on ID.
    -Delete: Finds a Booking based on ID.  Redirects to root path on successful deletion.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,60 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @cyclist = Cyclist.find(session[:id])
+    @booking = @cyclist.bookings.build(booking_params)
+    @booking.status = "Pending"
+
+    if @booking.save
+      create_services(params[:services])
+      redirect_to cyclist_bookings_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+
+    if @booking.update_attributes(booking_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def delete
+    @booking = Booking.find(params[:id])
+
+    if @booking.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  protected
+
+  def booking_params
+    params.require(:booking).permit(:mechanic_id, :description, :status)
+  end
+
+  def create_services(service_list)
+    for service in service_list
+      @booking.requested_services.build(booking_id: @booking.id, service_id: service)
+    end
+  end
+
 end
