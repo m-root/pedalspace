a7d6247d058d823736bd814d589e1ebc223390f2
jairusstreight <jairus@uvic.ca>
2016-07-15 20:08:51 -0700

    Remove the email column from Migration
    The email column already existed so, need to modify in order to rails db:migrate.

--- db/migrate/20160716025337_add_devise_to_users.rb
+++ db/migrate/20160716025337_add_devise_to_users.rb
@@ -2,7 +2,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.0]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      #t.string :email,              null: false, default: "" # Already has email column
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
