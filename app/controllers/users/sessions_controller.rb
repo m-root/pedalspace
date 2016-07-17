15fad2141dbdceb94af13ec8aa57ca657cdc90cd
jairusstreight <jairus@uvic.ca>
2016-07-16 20:04:21 -0700

    Fixed nav bar appearance, buttons rendering views
    Managed to add code into the render: :partial that prevented the nav bar from being viewed in the /sign_up and /sign_in views
    
    In future refactoring, the partial views should be rendered for each page, as some may have seperately formated nav bars
    
    Also , the landing page buttons where linked to the /sign_up and /sign_in routes

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,20 +1,20 @@
 class Users::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
 
-  GET /users/sign_in
-  def new
-    super
-  end
+  # GET /users/sign_in
+  # def new
+  #   super
+  # end
 
-  POST /users/sign_in
-  def create
-    super
-  end
+  # POST /users/sign_in
+  # def create
+  #   super
+  # end
 
-  DELETE /resource/sign_out
-  def destroy
-    super
-  end
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
 
   # protected
 
