122f1f859aaf5d5ddcc6dfcb79e3be943be672ee
Tana Jukes <tana.jukes@gmail.com>
2016-07-18 15:00:00 -0700

    Refactor mechanic average rating callback
    [Same refactor as for Cyclist average rating] Instead of using custom code to calculate a Mechanic's average rating every time a new review is created for them, this refactored method uses ActiveRecord's built in 'average' method. Using this built-in method makes for simpler and more reliable code.

--- app/models/review_of_mechanic.rb
+++ app/models/review_of_mechanic.rb
@@ -9,21 +9,19 @@ class ReviewOfMechanic < Review
 
   def service_expected_exists
     if service_expected.nil?
-      errors.add(:service_expected, "must exist") 
+      errors.add(:service_expected, "must exist")
     end
   end
 
   def price_expected_exists
     if price_expected.nil?
-      errors.add(:price_expected, "must exist") 
+      errors.add(:price_expected, "must exist")
     end
   end
 
   def calculate_new_avg
     mechanic = self.mechanic
-    total_points = ReviewOfMechanic.where(mechanic_id: mechanic.id).sum(:rating).to_f
-    number_of_reviews = ReviewOfMechanic.where(mechanic_id: mechanic.id).count
-    mechanic.avg_rating = total_points / number_of_reviews
+    mechanic.avg_rating = ReviewOfMechanic.where(mechanic_id: mechanic.id).average(:rating)
     mechanic.save
   end
 
