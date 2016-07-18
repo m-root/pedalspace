b7f04c3504a5b5727eebeac420d493acdaafce2d
Trevor Morrison <trevorjames.morrison@gmail.com>
2016-07-18 18:22:03 +0000

    Add calculate average rating callback to ReviewOfCyclist model
    Whenever a review written about a Cyclist is saved, a callback method on the ReviewOfCyclist model recalculates and sets the Cyclist's avg_rating.  The seeds.rb file no longer explicitly sets the avg_rating value on instantiation.

--- app/models/review_of_cyclist.rb
+++ app/models/review_of_cyclist.rb
@@ -5,10 +5,20 @@ class ReviewOfCyclist < Review
 
   validate :did_pay_exists
 
+  after_save :calculate_new_avg
+
   def did_pay_exists
     if did_pay.nil?
       errors.add(:did_pay, "must exist") 
     end
+  end
+
+  def calculate_new_avg
+    cyclist = self.cyclist
+    total_points = ReviewOfCyclist.where(cyclist_id: cyclist.id).sum(:rating).to_f
+    number_of_reviews = ReviewOfCyclist.where(cyclist_id: cyclist.id).count
+    cyclist.avg_rating = total_points / number_of_reviews
+    cyclist.save
   end
 
 end
