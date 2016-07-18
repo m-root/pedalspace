15ce3a5305586b0d169ed6a17918290c350e235a
Tana Jukes <tana.jukes@gmail.com>
2016-07-18 14:58:37 -0700

    Refactor cyclist average rating callback
    Instead of using custom code to calculate a Cyclist's average rating every time a new review is created for them, this refactored method uses ActiveRecord's built in 'average' method. Using this built-in method makes for simpler and more reliable code.

--- app/models/review_of_cyclist.rb
+++ app/models/review_of_cyclist.rb
@@ -9,15 +9,13 @@ class ReviewOfCyclist < Review
 
   def did_pay_exists
     if did_pay.nil?
-      errors.add(:did_pay, "must exist") 
+      errors.add(:did_pay, "must exist")
     end
   end
 
   def calculate_new_avg
     cyclist = self.cyclist
-    total_points = ReviewOfCyclist.where(cyclist_id: cyclist.id).sum(:rating).to_f
-    number_of_reviews = ReviewOfCyclist.where(cyclist_id: cyclist.id).count
-    cyclist.avg_rating = total_points / number_of_reviews
+    cyclist.avg_rating = ReviewOfCyclist.where(cyclist_id: cyclist.id).average(:rating)
     cyclist.save
   end
 
