1e06889a0311cc345e30e17f2c50b2cca795a6c0
Tana Jukes <tana.jukes@gmail.com>
2016-07-18 22:15:00 -0700

    Add basic Elastic settings to Mechanic model
    This commit adds basic ElasticSearch indexing settings. Most of the code added is example code from the docs - the only change was to change the column being indexed to :first_name from the default (:title). This code tells Elastic to index the first_name column and use the 'english' analyzer.

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -15,6 +15,12 @@ class Mechanic < User
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
 
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :first_name, analyzer: 'english', index_options: 'offsets'
+    end
+  end
+
 end
 
 Mechanic.import
