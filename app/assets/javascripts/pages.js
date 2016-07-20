1128994fdc5d937841bd8f4b55320cf3ebdcc265
Tana Jukes <tana.jukes@gmail.com>
2016-07-20 08:48:03 -0700

    Abstract map loading to JS file
    This commit removes the script to set up the map from the document head section and puts it in a separate Javascript file (pages.js was autogenerated by Rails and therefore does not need to be added to the asset pipeline).

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,10 +1,45 @@
- function initMap() {
-        var mapDiv = document.getElementById('map');
-        var map = new google.maps.Map(mapDiv, {
-            center: {lat: 44.540, lng: -78.546},
-            zoom: 8
-        });
-      }
+// Note: This code requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 48.4284, lng: -123.3656},
+    zoom: 12
+  });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+}
+
+
+// PREVIOUS CODE
 
       $(function() {
       	$('.ui.rating').rating(
