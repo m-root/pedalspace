58d853c2723399a1f3a4fd53a8bd12cf8ca060ab
Tana Jukes <tana.jukes@gmail.com>
2016-07-20 15:43:52 -0700

    Allow pages controller to return JSON
    This commit allows the Pages controller to return JSON from the #home action, as well as HTML/erb - depending on the request type. This will be used by the map script to retrieve the mechanics' coordinates as stored in the database in order to display them on the map.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def home
     @mechanics = Mechanic.all
+
+    respond_to do |format|
+      format.html # home.html.erb
+      format.json { render json: @mechanics }
+    end
   end
 end
