74d979841893b97be34ade90d1f2447096729b69
Tana Jukes <tana.jukes@gmail.com>
2016-07-20 20:36:07 -0700

    Add search functionality to Pages controller
    Because the search results will be displayed at /pages/home, the Pages controller needs to be able to handle search requests. For simplicity's sake, this commit adds search handling directly to the Pages#home action. The search handling code is copied directly from the Mechanics controller where it was originally defined, with two small changes:
    - Add a check for 'location' in the params
    - Permit 'location' in the strong params

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,22 @@
 class PagesController < ApplicationController
   def home
-    @mechanics = Mechanic.all
+
+    if params[:location].present? || params[:city].present? || params[:service].present? || params[:price].present?
+      @mechanics = Mechanic.search(search_params)
+    else
+      @mechanics = Mechanic.all
+    end
 
     respond_to do |format|
       format.html # home.html.erb
       format.json { render json: @mechanics }
     end
   end
+
+  protected
+
+  def search_params
+    params.permit(:location, :city, :service, :price)
+  end
+
 end
